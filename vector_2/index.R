9c2b2aac335400b12282343467d0e0a056ae74ab
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-04 17:00:16 +0200

    INDEX: demonstrate indexing a vector using integers

--- vector_2/index.R
+++ vector_2/index.R
@@ -14,3 +14,14 @@ y <- c(1:3, NA, 5:2, NaN)
 print(y)
 print("y[!is.na(y)]")
 print(y[!is.na(y)])
+
+print("Demonstrate the usage of a positive integral vector")
+x <- 1:10
+print("1:10")
+print(x)
+i <- c(2, 3, 6, 7, 8)
+print(paste0("Indices: ", i))
+
+print("x[i]")
+out <- x[i]
+print(out)
