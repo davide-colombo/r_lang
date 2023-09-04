53a68130a0545f4bd5ffd82aef99d7ceec595b5c
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-04 18:39:48 +0200

    FACTOR: demonstrate how to cope with NA values

--- vector_2/factor.R
+++ vector_2/factor.R
@@ -13,3 +13,23 @@ print(length(f))
 print("Inspecting the attributes of a factor object")
 print(paste0("levels(f) = ", levels(f)))
 print(paste0("class(f) = ", class(f)))
+
+print("Demonstrate the usage of factor objects with NA values")
+nums <- c(1, 2, NA, 4, 5, NA, 7, 8)
+print(nums)
+print(paste("mode(nums) = ", mode(nums), ", storage.mode(nums) = ", storage.mode(nums)))
+
+# This considers NA values as a new level
+ff <- factor(nums, exclude=NULL)
+print(paste("levels(ff) = ", levels(ff)))
+
+print("==================================================")
+print("Before setting NA values")
+print(paste("is.na(ff) = ", is.na(ff)))
+
+# The problem with this is that now the 'is.na()' function is broken
+is.na(ff)[is.na(nums)] <- TRUE
+
+print("==================================================")
+print("After having set NA values properly")
+print(paste("is.na(ff) = ", is.na(ff)))
