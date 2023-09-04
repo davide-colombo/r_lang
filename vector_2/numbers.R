5a13737813aaae216b2aca9b19717aa9b2cb9938
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-04 15:42:21 +0200

    NUMBERS: simple example on how to create vector of numbers

---
+++ vector_2/numbers.R
@@ -0,0 +1,13 @@
+# Simple example on numeric vectors
+
+
+print("Using the ':' operator")
+print("v1 <- 1:4")
+v1 <- 1:4
+print(v1)
+
+print()
+print("Using the 'c()' function")
+print("v2 <- c(10, 9, 13, 13, 13, 5, 5, 89, 0, -32)")
+v2 <- c(10, 9, 13, 13, 13, 5, 5, 89, 0, -32)
+print(v2)
