83201a208423b58f92b11901cc12bde03a5f9903
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-04 17:05:16 +0200

    NAMES: demonstrate how to use "name" attributes

---
+++ vector_2/names.R
@@ -0,0 +1,10 @@
+# Demonstration of the usage of the attribute "names"
+
+print("Calories")
+calories <- c(113, 80, 256)
+print(calories)
+
+print("Setting names to calories")
+names(calories) <- c("apple", "pear", "banana")
+print(calories)
+
