ef6cb8f868995dc97920d87d8c0a2fd5bc7db704
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-05 10:18:48 +0200

    MATRIX: demonstrate column major ordering

--- vector_2/matrix.R
+++ vector_2/matrix.R
@@ -9,3 +9,17 @@ print("Now assign the dim() attribute of z")
 print("dim(z) <- c(2, 5)")
 dim(z) <- c(2, 5)
 print(paste0("dim(z) = ", dim(z)))
+
+z[1, 1] <- 1
+z[2, 1] <- 2
+z[1, 2] <- 3
+z[2, 2] <- 4
+z[1, 3] <- 5
+z[2, 3] <- 6
+z[1, 4] <- 7
+z[2, 4] <- 8
+z[1, 5] <- 9
+z[2, 5] <- 10
+
+print("COLUMN MAJOR ORDERING")
+print(z)
