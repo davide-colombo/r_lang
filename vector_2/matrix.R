c82279caec297a48498f56a45c08ad5830a6430a
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-05 10:30:41 +0200

    MATRIX: demonstrate indexing on multi dimensional vector

--- vector_2/matrix.R
+++ vector_2/matrix.R
@@ -23,3 +23,20 @@ z[2, 5] <- 10
 
 print("COLUMN MAJOR ORDERING")
 print(z)
+
+# Consider a 3D array
+print("a <- array(1:24, dim=c(3, 4, 2))")
+a <- array(1:24, dim=c(3, 4, 2))
+print(a)
+
+# Now let's take the second row from 'a'
+print("a[2,,]")
+r <- a[2,,]
+print("dim(a[2,,])")
+print(dim(r))
+
+# The multi-dimensional vector 'r' contains
+# the values of 'a' in this order:
+# c(a[2,1,1], a[2,2,1], a[2,3,1], a[2,4,1],
+#   a[2,1,2], a[2,2,2], a[2,3,2], a[2,4,2])
+
