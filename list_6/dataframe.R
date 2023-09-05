446bb79ffa83cf67ef195d41b13c68bbd427c3be
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-05 17:00:20 +0200

    LIST_6/ : demonstrated basic properties of a data frame object

---
+++ list_6/dataframe.R
@@ -0,0 +1,14 @@
+# Test how to use Data frame objects
+
+library(dplyr)          # filter() function
+
+df <- data.frame(
+    x = c(1, 2, 3, 4),
+    y = c("a", "b", "c", "d")
+    )
+
+print(df)
+print(mode(df))
+print(storage.mode(df))
+print(typeof(df))
+print(format(object.size(df), units='auto'))
