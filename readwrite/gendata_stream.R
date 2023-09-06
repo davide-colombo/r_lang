5dd25475f279e48b0f873419e844214a1285c309
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-06 15:05:26 +0200

    YAML: demonstrated how to take program parameters from a config file

--- readwrite/gendata_stream.R
+++ readwrite/gendata_stream.R
@@ -1,36 +1,43 @@
 # Generate synthetic data
 # Store into a data frame object
 # Write to a file
+library(yaml)
 
-odir = './ioexamples'
+conf_path = './config/genstream.yml'
+if(!file.exists(conf_path)){
+    # TODO: log info
+    stop("Missing configuration file at: ", conf_path)
+}
+
+config = yaml.load_file(conf_path)
+
+output = config$output
+tokensep = output$sep
+odir = output$dirname
 if(!dir.exists(odir))
     dir.create(odir)
 
-fname = 'synthetic_genes2.csv'
+fname = output$fname
 opath = file.path(odir, fname)
 if(!file.exists(opath))
     file.create(opath)
 
-# TODO: these params can be read from a config file
-set.seed(123456)
-
-max_gene_number <- 1000
-max_chro_number <- 3
-max_expl_number <- 200
-
-# TODO: read from arguments
-howmany <- 554
+param = config$param
+set.seed(param$seed)
+howmany = param$howmany
+max_gene_num = param$max_gene_num
+max_chro_num = param$max_chro_num
+max_gene_exp = param$max_gene_exp
 
 fw <- file(opath, "wt")
 for(i in 1:howmany){
-    explevel <- sample(0:max_expl_number, 1)
-    genenum <- sample(1:max_gene_number, 1)
-    chronum <- sample(1:max_chro_number, 1)
+    explevel <- sample(0:max_gene_exp, 1)
+    genenum <- sample(1:max_gene_num, 1)
+    chronum <- sample(1:max_chro_num, 1)
     genename <- paste0('g_', genenum)
     chroname <- paste0('c_', chronum)
-    writeLines(
-        paste(chronum, chroname, genenum, genename, explevel, sep=','),
-        fw
-    )
+
+    oline <- paste(chronum, chroname, genenum, genename, explevel, sep=tokensep)
+    writeLines(oline, fw)
 }
 close(fw)
