f6f77e115e08855cfa4df62669ad4d1924a81009
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-06 18:05:12 +0200

    FILEREAD: set number of core used by data.table package

--- readwrite/fileread.R
+++ readwrite/fileread.R
@@ -3,6 +3,11 @@
 library(data.table)
 library(yaml)
 
+# cat(sprintf("number of threads used: %d\n", getDTthreads(verbose=TRUE)))
+# Set the number of threads used by data.table as the total number of cores
+# https://rdatatable.gitlab.io/data.table/reference/openmp-utils.html
+setDTthreads(threads=0)
+
 configpath = './config/genstream.yml'
 if(!file.exists(configpath)){
     stop("Cannot find configuration file at: ", configpath)
@@ -17,7 +22,7 @@ if(!file.exists(ifpath)){
 }
 
 # Read the entire CSV file into a data frame structure
-rawdata = fread(ifpath)
+rawdata = fread(ifpath, colClasses=list(character=c(2, 4), integer=c(1, 3, 5)))
 cat(sprintf("mode(rawdata) = %s\n", mode(rawdata)))
 cat(sprintf("size = %s\n", format(object.size(rawdata), units='auto')))
 cat(sprintf("rows = %d, columns = %d\n", nrow(rawdata), ncol(rawdata)))
