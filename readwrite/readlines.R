5928fb46a45ad9cb6b0b7cfda82831c8152a5017
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-06 10:10:43 +0200

    READWRITE: demonstrate how to open and close a connection to file

--- readwrite/readlines.R
+++ readwrite/readlines.R
@@ -3,13 +3,18 @@
 odir = './ioexamples'
 
 if(!dir.exists(odir)){
+    cat(sprintf("Creating directory: %s\n", odir))
     dir.create(odir)
 }
 
 fn <- file.path(odir, 'ex.data')
 if(!file.exists(fn)){
-    cat("wrote file: ", fn, "\n")
-    cat("TITLE extra line", "2 3 5 7", "", "11 13 17\n", file=fn)
+    cat(sprintf("Creating file %s\n", fn))
+
+    ff <- file(fn, 'w')     # open the file in write mode
+    cat("TITLE extra line", "2 3 5 7", "", "11 13 17", file=ff, sep='\n')
+    cat("One more line\n", file=ff)
+    close(ff)
 }
 
 # Read the entire file
@@ -19,3 +24,11 @@ readLines(fn, n = -1)   # read the whole file
 # NOTE: readLines() throws an error if the last line
 #       does not contain a '\n'!!!
 
+print("Demonstrate how to open a file connection")
+con <- file(fn, 'r')
+
+if(con){
+    print("Connection successfully opened")
+}
+
+close(con)
