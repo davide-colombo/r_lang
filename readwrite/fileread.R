14ee27627a58155f8405c365b4f3beda4348d020
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-06 18:35:25 +0200

    FILEREAD: demonstrated how to use a data.table object to make statistics

--- readwrite/fileread.R
+++ readwrite/fileread.R
@@ -24,6 +24,26 @@ if(!file.exists(ifpath)){
 # Read the entire CSV file into a data frame structure
 rawdata = fread(ifpath, colClasses=list(character=c(2, 4), integer=c(1, 3, 5)))
 cat(sprintf("mode(rawdata) = %s\n", mode(rawdata)))
+cat(sprintf("class(rawdata) = %s\n", class(rawdata)))
 cat(sprintf("size = %s\n", format(object.size(rawdata), units='auto')))
 cat(sprintf("rows = %d, columns = %d\n", nrow(rawdata), ncol(rawdata)))
 head(rawdata)
+
+# https://cran.r-project.org/web/packages/data.table/vignettes/datatable-intro.html
+# DT [i,        j,          by      ]
+# DT [where,    select,     group by]
+
+print("Compute the sample mean of #reads grouped by chromosome name")
+stat1 <- rawdata[, mean(read_count), by = chromosome_name]
+print(stat1)
+
+stat2 <- rawdata[chromosome_id == 1, mean(read_count), by = gene_name]
+print("Compute the sample mean of #reads from chromosome 1 grouped by gene name")
+print(stat2)
+
+genesubset <- c(33, 56, 767, 331)
+stat3 <- rawdata[gene_id %in% genesubset,
+                 list("mean"=mean(read_count), "count"=length(read_count)),
+                 by = gene_name]
+print("Compute the sample mean of #reads for a subset of genes grouped by gene name")
+print(stat3)
