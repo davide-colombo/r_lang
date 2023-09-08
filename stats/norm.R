c5e64f288f17b39f49255509302a84dc86539706
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-08 09:58:39 +0200

    NORM.R: tweak the hist() function

--- stats/norm.R
+++ stats/norm.R
@@ -6,7 +6,7 @@ true_mean <- 37.9
 true_sd <- 4.56
 
 # Generate random samples from a normal distribution
-nsamples <- 30
+nsamples <- 15
 rawX <- rnorm(nsamples, mean = true_mean, sd=true_sd)
 
 cat(sprintf("length: %d\n", length(rawX)))
@@ -15,6 +15,13 @@ cat(sprintf("storage.mode: %s\n", storage.mode(rawX)))
 
 odir_plot <- "plots"
 opath <- file.path(odir_plot, 'hist_raw.pdf')
+title=paste('Histogram of', nsamples,
+            'samples from a Normal distribution, mean=',
+            true_mean, 'sd=', true_sd)
+
+myxlim=c(round(min(rawX)-10), round(max(rawX)+10))
 pdf(opath, onefile=TRUE)
-hist(rawX)
+hist(rawX, main=title, freq=FALSE, xlim=myxlim, col='#FF6600')
+curve(dnorm(x, mean=mean(rawX), sd=sd(rawX)), add=TRUE, col='#0066FF')
+curve(dnorm(x, mean=true_mean, sd=true_sd), add=TRUE, col='#00EE66')
 dev.off()
