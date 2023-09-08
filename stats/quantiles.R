0766d6ea19617dcfe3501852f4c8d991e67571a2
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-08 13:14:21 +0200

    QUANTILES.R: try min-max normalization in quantile computation

--- stats/quantiles.R
+++ stats/quantiles.R
@@ -1,7 +1,7 @@
 # Demonstrate how to compute quantiles
 
-n <- 100
-x <- sample(x=23:89, size=n, replace=TRUE)
+r <- 23:89
+x <- sample(x=r, size=length(r), replace=FALSE)
 
 quartiles_q <- quantile(x, probs=seq(0, 1, 0.25))
 print("Quartiles: ")
@@ -11,7 +11,16 @@ deciles_q <- quantile(x, probs=seq(0, 1, 0.1))
 print("Deciles: ")
 print(deciles_q)
 
-opath <- 'plots/hist_quantiles.pdf'
-pdf(opath)
-hist(x, col='#00FF66', main=paste('Random sample with replacement, n =', n))
-dev.off()
+xs = sort(x)
+print(xs)
+x_norm = (xs - min(x)) / (max(x) - min(x))
+print(x_norm)
+probs = seq(0, 1, 0.25)
+indices = sapply(probs, function(p) which.min(abs(x_norm-p)))
+q = xs[indices]
+print(q)
+
+# opath <- 'plots/hist_quantiles.pdf'
+# pdf(opath)
+# hist(x, col='#00FF66', main=paste('Random sample with replacement, n =', n))
+# dev.off()
