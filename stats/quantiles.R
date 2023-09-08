3702c6edd86ec884e10e547055d604dd22c9e108
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-08 17:08:22 +0200

    QUARTILES.R: plotted the frequency, relative frequency, cumulative and relative cumulative

--- stats/quantiles.R
+++ stats/quantiles.R
@@ -24,7 +24,7 @@
 # with repeated samples.
 set.seed(123456)
 
-n = 20
+n = 500
 true_mu = 42.1
 true_sd = 5.67
 
@@ -53,20 +53,33 @@ xcrf <- cumsum(xf) / n
 # cat(sprintf("%s\n", paste('x =', round(xu, 2), 'abs =', xf, 'rel =', xrf, 'cum =', xcf, 'crel =', xcrf, sep='\t')))
 
 opath <- 'plots/combined_x.pdf'
-pdf(opath)
+xf_ylim = c(0, round(max(xf)+5))
+xrf_ylim = c(0, max(xrf))
+xcf_ylim = c(0, round(max(xcf)))
+xcrf_ylim = c(0, ceiling(max(xcrf)))
+
+pdf(opath, onefile=TRUE)
 par(mfrow=c(2, 2))
 # hist(x, col='#00FF66', main=paste('Absolute frequency (n =', n, ')'))
 # hist(x, col='#0066FF', main=paste('Relative frequency (n =', n, ')'), freq=FALSE)
-plot(xu, xf)
-plot(xu, xrf)
-plot(xu, xcf)
-plot(xu, xcrf)
+plot(xu, xf, cex=0.5, main=paste('Frequency (n =', n, ')'), xlab='X [units]', ylab='Frequency', ylim=xf_ylim, xaxs='i', yaxs='i')
+axis(1, at=xu)  # bottom axis
+axis(2, at=xf)  # left axis
+curve(n*dnorm(x, mean=mean(x), sd=sd(x)), add=TRUE, col='#FF6600', lty=2)
+
+plot(xu, xrf, cex=0.5, main=paste('Relative frequency (n =', n, ')'), xlab='X [units]', ylab='Relative frequency', ylim=xrf_ylim)
+curve(dnorm(x, mean=mean(x), sd=sd(x)), add=TRUE, col='#FF6600', lty=2)
+
+plot(xu, xcf, cex=0.5, main=paste('Cumulative frequency (n =', n, ')'), xlab='X [units]', ylab='Cumulative frequency', ylim=xcf_ylim)
+curve(n*pnorm(x, mean=mean(x), sd=sd(x)), add=TRUE, col='#FF6600', lty=2)
+
+plot(xu, xcrf, cex=0.5, main=paste('Relative cumulative frequency (n =', n, ')'), xlab='X [units]', ylab='Relative cumulative frequency', ylim=xcrf_ylim)
 curve(pnorm(x, mean=mean(x), sd=sd(x)), add=TRUE, col='#FF6600', lty=2)
+
 dev.off()
 
 # Rank of the data
 xrk <-rank(x)
-print(xrk)
 
 # # Using the quantile() function to compute quartiles
 # probs = seq(0, 1, 0.25)
